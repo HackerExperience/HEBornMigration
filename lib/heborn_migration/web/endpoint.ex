dc430f7da413c31f23df1c986c4f0c2e69484227
kress95 <cferraz95@gmail.com>
2017-04-17 19:35:05 -0300

    Remove default channel (it's not being used anywhere).

--- lib/heborn_migration/web/endpoint.ex
+++ lib/heborn_migration/web/endpoint.ex
@@ -1,18 +1,10 @@
 defmodule HEBornMigration.Web.Endpoint do
   use Phoenix.Endpoint, otp_app: :heborn_migration
 
-  socket "/socket", HEBornMigration.Web.UserSocket
-
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phoenix.digest
-  # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :heborn_migration, gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
-  # Code reloading can be explicitly enabled under the
-  # :code_reloader configuration of your endpoint.
   if code_reloading? do
     socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
     plug Phoenix.LiveReloader
@@ -30,9 +22,6 @@ defmodule HEBornMigration.Web.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
 
-  # The session will be stored in the cookie and signed,
-  # this means its contents can be read but not tampered with.
-  # Set :encryption_salt if you would also like to encrypt it.
   plug Plug.Session,
     store: :cookie,
     key: "_heborn_migration_key",
@@ -40,15 +29,10 @@ defmodule HEBornMigration.Web.Endpoint do
 
   plug HEBornMigration.Web.Router
 
-  @doc """
-  Dynamically loads configuration from the system environment
-  on startup.
-
-  It receives the endpoint configuration from the config files
-  and must return the updated configuration.
-  """
   def load_from_system_env(config) do
-    port = System.get_env("PORT") || raise "expected the PORT environment variable to be set"
+    port =
+      System.get_env("PORT")
+      || raise "expected the PORT environment variable to be set"
     {:ok, Keyword.put(config, :http, [:inet6, port: port])}
   end
 end

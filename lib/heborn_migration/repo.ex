97e0459c3eb09510a3c9d6ad1057031dc144f52f
kress95 <cferraz95@gmail.com>
2017-04-17 19:38:18 -0300

    Remove default comments from Phoenix boilerplate.

--- lib/heborn_migration/repo.ex
+++ lib/heborn_migration/repo.ex
@@ -1,10 +1,6 @@
 defmodule HEBornMigration.Repo do
   use Ecto.Repo, otp_app: :heborn_migration
 
-  @doc """
-  Dynamically loads the repository url from the
-  DATABASE_URL environment variable.
-  """
   def init(_, opts) do
     {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
   end

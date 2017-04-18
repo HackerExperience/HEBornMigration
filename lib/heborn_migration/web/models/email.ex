1c1fc718b840b7ee4567ae5c0da1b9d6be9435c4
kress95 <cferraz95@gmail.com>
2017-04-18 20:05:09 -0300

    Using template to compose the confirmation email.

--- lib/heborn_migration/web/models/email.ex
+++ lib/heborn_migration/web/models/email.ex
@@ -1,27 +1,29 @@
 defmodule HEBornMigration.Web.Email do
 
   alias HELF.Mailer
+  alias HEBornMigration.Web.LayoutView
+  alias HEBornMigration.Web.PageView
+
+  import Phoenix.View, only: [render_to_string: 3]
 
   @url "1.hackerexperience.com/migrate"
 
-  # FIXME: use a template for both html_message and text_message
   def confirmation(to, code) do
     link = @url <> "/confirm"
-    link_with_code = @url <> "/confirm/#{code}"
 
-    html_message = """
-    <p>Click <a href="#{link_with_code}">here</a> to confirm your account</p>
-    <p>
-      Trouble with the link?<br/>
-      Access #{link} and use the following code to confirm your account:
-    </p>
-    <pre>#{code}</pre>
-    """
+    assigns = [
+      code: code,
+      link: link,
+      link_with_code: link <> "/#{code}",
+      layout: {LayoutView, "email.html"}
+    ]
+
+    html_message = render_to_string(PageView, "confirmation_email.html", assigns)
 
     text_message = """
-    Access #{link} and use the following code to confirm your account:
-    #{code}
+    Access [#{link}] and confirm your account with the following code: [#{code}]
     """
+    {LayoutModule, "template.extension"}
 
     Mailer.new()
     |> Mailer.to(to)
